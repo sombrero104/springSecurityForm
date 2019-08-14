3c8b4240ae04cb40f7a4107bbe6a6676876e9a79
sombrero104 <sombrero.khy@gmail.com>
2019-08-14 17:16:57 +0900

    .

--- src/main/java/sombrero/Application.java
+++ src/main/java/sombrero/Application.java
@@ -2,9 +2,29 @@ package sombrero;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class Application {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        /**
+         * return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+         *  => 기본적으로 bcrypt 사용.
+         * http://localhost:8080/account/USER/sombrero/123 결과:
+         * {
+         *      "id":1
+         *      ,"username":"sombrero"
+         *      ,"password":"{bcrypt}$2a$10$Wgmkst4CHTIzfCGISlV5A.68m7l6HSCOAYxyPhtU0AvGrjTX85PSW"
+         *      ,"role":"USER"
+         *  }
+         */
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
