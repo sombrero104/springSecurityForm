0f857fc2b41753cc0b6bfe44fe8241a303a4cdf7
sombrero104 <sombrero.khy@gmail.com>
2019-08-14 22:21:12 +0900

    .

--- src/main/java/sombrero/form/SampleService.java
+++ src/main/java/sombrero/form/SampleService.java
@@ -14,18 +14,18 @@ public class SampleService {
      * Spring Security의 구조.
      * SecurityContextHolder > SecurityContext > Authentication
      *
-     * SecurityContextHolder
+     * 1. SecurityContextHolder
      *  -> SecurityContext 제공, 기본적으로 ThreadLocal을 사용한다. (하나의 스레드에서 자원 공간을 공유하 방식.)
      *                          한 스레드에 특화되어 있는 정보. 한 스레드 내에서는 어디에서나 접근 가능. 스레드가 다를 경우 같은 인증 정보를 가져올 수 없음.
      *                          ThreadLocal 외에 다른 전략 사용 필요.
      *                          async하게 threadpool을 사용하지 않는 이상 서블릿은 thread per request(스레드 하나 = 요청 하나)이므로 기본적으로 ThreadLocal 사용.
-     * SecurityContext
+     * 2. SecurityContext
      *  -> Authentication 제공.
      */
     public void dashboard() {
 
         /**
-         * authentication: Principal과 GrantAuthority 제공.
+         * 3. authentication: Principal과 GrantAuthority 제공.
          *
          * 디버깅 결과:
          * org.springframework.security.authentication.UsernamePasswordAuthenticationToken@e27597c5: Principal:
@@ -38,7 +38,7 @@ public class SampleService {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         /**
-         * principal: 인증한 사용자를 나타내는 정보.
+         * 4. principal: 인증한 사용자를 나타내는 정보.
          *            UserDetailsService에서 리턴한 UserDetails 타입의 객체.
          *
          *            * UserDetails: 애플리케이션이 가지고 있는 유저 정보와 시큐리티가 사용하는 Authentication 객체 사이의 어댑터.
@@ -53,7 +53,7 @@ public class SampleService {
         Object principal = authentication.getPrincipal();
 
         /**
-         * authorities(GrantedAuthority): "ROLE_USER", "ROLE_ADMIN" 등 사용자가 가지고 있는 권한.
+         * 5. authorities(GrantedAuthority): "ROLE_USER", "ROLE_ADMIN" 등 사용자가 가지고 있는 권한.
          *                                인증 이후, 인가 및 권한을 확인할 때 이 정보를 참조한다.
          *                                사용자가 가지고 있는 권한이 여러개일 수도 있으므로 컬렉션 타입.
          *
@@ -65,7 +65,7 @@ public class SampleService {
         Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
 
         /**
-         * credentials: 인증할 때만 사용. (인증을 한 다음에는 값을 가지고 있을 필요가 없기 때문에 현재는 값이 없음.)
+         * 6. credentials: 인증할 때만 사용. (인증을 한 다음에는 값을 가지고 있을 필요가 없기 때문에 현재는 값이 없음.)
          *
          * 디버깅 결과:
          * credentials = null
@@ -73,7 +73,7 @@ public class SampleService {
         Object credentials = authentication.getCredentials();
 
         /**
-         * authenticated: 인증된 사용자인지 나타내는 정보.
+         * 7. authenticated: 인증된 사용자인지 나타내는 정보.
          *
          * 디버깅 결과:
          * authenticated = true
