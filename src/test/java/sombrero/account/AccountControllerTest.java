8600ad821b941203875f7fc766d8790d8ba1f618
sombrero104 <sombrero.khy@gmail.com>
2019-08-14 21:19:34 +0900

    .

--- src/test/java/sombrero/account/AccountControllerTest.java
+++ src/test/java/sombrero/account/AccountControllerTest.java
@@ -9,10 +9,14 @@ import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.Assert.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -24,6 +28,9 @@ public class AccountControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    AccountService accountService;
 
     /**
      * (1-1) anonymous
@@ -98,6 +105,57 @@ public class AccountControllerTest {
         mockMvc.perform(get("/admin").with(user("admin").roles("ADMIN")))
                 .andDo(print())
                 .andExpect(status().isOk());
+    }
+
+    /**
+     * form 로그인 테스트 (성공하는 경우).
+     */
+    @Test
+    @Transactional
+    public void login_success() throws Exception {
+        String username = "sombrero";
+        String password = "123";
+        Account user = this.createUser(username, password);
+        mockMvc.perform(formLogin().user(username).password(password))
+                .andExpect(authenticated());
+    }
+
+    /**
+     * 여러개의 테스트에서 같은 사용자 추가 시 중복 에러 발생.
+     *  => 각 테스트마다 트랜잭션을 추가하여 실패 시 롤백되도록 설정하면 다른 테스트에 영향을 주지 않음.
+     */
+    @Test
+    @Transactional
+    public void login_success2() throws Exception {
+        String username = "sombrero";
+        String password = "123";
+        Account user = this.createUser(username, password);
+        mockMvc.perform(formLogin().user(username).password(password))
+                .andExpect(authenticated());
+    }
+
+    /**
+     * form 로그인 테스트 (실패하는 경우).
+     */
+    @Test
+    @Transactional
+    public void login_fail() throws Exception {
+        String username = "sombrero";
+        String password = "123";
+        Account user = this.createUser(username, password);
+        mockMvc.perform(formLogin().user(username).password("12345"))
+                .andExpect(unauthenticated());
+    }
+
+    /**
+     * 테스트 유저 생성.
+     */
+    private Account createUser(String username, String password) {
+        Account account = new Account();
+        account.setUsername(username);
+        account.setPassword(password);
+        account.setRole("USER");
+        return accountService.createNew(account);
     }
 
 }
