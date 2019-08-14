9b465d2da169e234f21a98b5489dc430fa59bbb9
sombrero104 <sombrero.khy@gmail.com>
2019-08-14 19:35:37 +0900

    .

--- src/test/java/sombrero/account/AccountControllerTest.java
+++ src/test/java/sombrero/account/AccountControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -24,6 +25,7 @@ public class AccountControllerTest {
     MockMvc mockMvc;
 
     /**
+     * (1-1) anonymous
      * 로그인을 하지 않은 상태에서 페이지 응답이 어떻게 나오는지 테스트.
      */
     @Test
@@ -34,11 +36,43 @@ public class AccountControllerTest {
     }
 
     /**
+     * (1-2) anonymous 애노테이션 방법
+     */
+    @Test
+    @WithAnonymousUser
+    public void index_anonymous_annotation() throws Exception {
+        mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    /**
+     * (2) 인덱스 페이지에 USER 권한 사용자가 로그인한 경우.
      * 임의로 만든 유저(실제 존재하지 않는 mock 유저)가 로그인한 상태에서 페이지 응답이 어떻게 나오는지 테스트.
      */
     @Test
     public void index_user() throws Exception {
         mockMvc.perform(get("/").with(user("sombrero").roles("USER")))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    /**
+     * (3-1) admin 페이지에 USER 권한 사용자가 로그인한 경우.
+     */
+    @Test
+    public void admin_user() throws Exception {
+        mockMvc.perform(get("/admin").with(user("sombrero").roles("USER")))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+    }
+
+    /**
+     * (3-2) admin 페이지에 ADMIN 권한 사용자가 로그인한 경우.
+     */
+    @Test
+    public void admin_admin() throws Exception {
+        mockMvc.perform(get("/admin").with(user("admin").roles("ADMIN")))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
