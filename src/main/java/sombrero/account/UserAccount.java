8b2838a766507b8aabff5b7faa93d933656a8c3b
sombrero104 <sombrero.khy@gmail.com>
2019-08-20 17:11:34 +0900

    .

--- src/main/java/sombrero/account/UserAccount.java
+++ src/main/java/sombrero/account/UserAccount.java
@@ -6,13 +6,16 @@ import org.springframework.security.core.userdetails.User;
 import java.util.List;
 
 /**
- * @AuthenticationPrincipal로 SecurityContextHolder 안에 있는 principal(UserDetails 객체) 가져오기.
- * (Controller에서 인자로 받고 있는 java.security.Principal를
- *      SecurityContextHolder 안에 있는 principal(UserDetails 객체)로 사용하도록 변경하기.)
- * (1) UserAccount.java 생성.
+ * java.security.Principal을 사용하지 않고,
+ * UserDetails 타입인
+ * Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+ * 를 본인 도메인이 제공하는(본인 도메인에서 사용자를 나타내는) 타입(Account)으로 사용하고 싶을 때.
+ * 커스텀한 User를 생성.
+ * (1) 스프링 시큐리티가 제공하는 User를 상속받는 UserAccount.java 생성.
  * (2) AccountService.java의 loadUserByUsername() 리턴 부분을
  *      return new UserAccount(account); 로 변경.
- * (3) Controller에서 '@AuthenticationPrincipal UserAccount userAccount'를 인자로 받을 수 있게 됨.
+ * (3) SampleController에서 '@AuthenticationPrincipal UserAccount userAccount'를 인자로 받을 수 있게 됨.
+ *     (SecurityContextHolder 안에 있는 principal(UserDetails 객체) 가져옴.)
  */
 public class UserAccount extends User {
 
